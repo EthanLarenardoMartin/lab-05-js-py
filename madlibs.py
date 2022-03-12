daa8c12604a3e38911b21f32e9c043e788e86b14
Ethan <ethan.martin@mail.citytech.cuny.edu>
2022-03-12 18:09:35 -0500

    completed the madlibs portion of lab 5 for javascript and python

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = " Pasta"
+adjective =" Happy"
+verb = " Goin'"
+adverb = "Loudly"
+
+print("On a tropical" + str( noun) + "," )
+print("Underneath a" + str( adjective) +  " moon.")
+print(str(adverb) + str( verb) + " with the hula dancers,")
+print("Askin' questions cause' they got all the" + str( noun) + "s.")
