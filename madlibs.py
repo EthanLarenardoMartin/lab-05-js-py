03bfb991caed6bddb0756a4f93cb6400dc8e3832
Ethan <ethan.martin@mail.citytech.cuny.edu>
2022-03-14 15:53:19 -0400

    added user input to both html and python

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,9 @@
-noun = " Pasta"
-adjective =" Happy"
-verb = " Goin'"
-adverb = "Loudly"
+noun = input(" Please enter a noun.")
+adjective = input(" Please enter an adjective.")
+verb = input(" Please enter a verb.")
+adverb = input("Please enter an adverb.")
 
 print("On a tropical" + str( noun) + "," )
 print("Underneath a" + str( adjective) +  " moon.")
-print(str(adverb) + str( verb) + " with the hula dancers,")
-print("Askin' questions cause' they got all the" + str( noun) + "s.")
+print("Hangin'" + str(adverb) + " with the hula dancers,")
+print(str(verb) + "Askin' questions cause' they got all the answers.")
